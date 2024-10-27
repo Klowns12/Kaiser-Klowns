c556eb9fda4bf7d1061b5b8e78d7986f4e47288e
Klowns12 <prawit.kaiser@gmail.com>
2024-10-27 22:50:30 +0700

    add file edit nav

---
+++ kaiser-klowns/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<nav class="bg-black p-4">
+  <div class="text-white text-lg font-bold">Kaiser Klowns</div>
+</nav>
+
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
